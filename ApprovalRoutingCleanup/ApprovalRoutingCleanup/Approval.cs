48098eb5a8f11a252903fb85d12e752c2785d327
danielramage <danielramage00@outlook.com>
2021-02-24 18:15:25 -0600

    Requirement 2 code

---
+++ ApprovalRoutingCleanup/ApprovalRoutingCleanup/Approval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApprovalRoutingCleanup
+{
+    public class Approval
+    {
+        public string LU_Name { get; set; }
+        public string Key_Ref { get; set; }
+        public string Step_No { get; set; }
+        public string Description { get; set; }
+        public string App_Date { get; set; }
+        public string Approval_Status { get; set; }
+        public string Prev_Approval_Date { get; set; }
+        public string Person_ID { get; set; }
+        public string Obj_ID { get; set; }
+    }
+}
